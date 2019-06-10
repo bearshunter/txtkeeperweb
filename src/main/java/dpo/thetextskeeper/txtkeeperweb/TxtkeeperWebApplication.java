004f05c55abbc3a2f4a7680649205234c9bdc9ef
dpo <test@mail.com>
2019-06-10 19:42:51 +0300

    springboot init

---
+++ src/main/java/dpo/thetextskeeper/txtkeeperweb/TxtkeeperWebApplication.java
@@ -0,0 +1,13 @@
+package dpo.thetextskeeper.txtkeeperweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxtkeeperWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TxtkeeperWebApplication.class, args);
+	}
+
+}
