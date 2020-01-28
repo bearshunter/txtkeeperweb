6831bd9a94f8064e8be2a23b33804a1e5c995cb4
Rodjerbear <51483121+Rodjerbear@users.noreply.github.com>
2020-01-28 10:41:20 +0200

    fix of create-drop database

--- src/main/java/dpo/thetextskeeper/txtkeeperweb/TxtkeeperWebApplication.java
+++ src/main/java/dpo/thetextskeeper/txtkeeperweb/TxtkeeperWebApplication.java
@@ -20,42 +20,6 @@ public class TxtkeeperWebApplication {
         SpringApplication.run(TxtkeeperWebApplication.class, args);
     }
 
-    @Bean
-    public CommandLineRunner demo(ArticleService articleService, PhraseService phraseService, UserService userService) {
-        return (args) -> {
-            User test = new User("test", "test@mail.com");
-            test.setId(42L);
-            final User savedUser = userService.save(test);
-            final Lesson lesson = new Lesson(savedUser.getId(), "Test German Lesson");
-            lesson.setCreated(System.currentTimeMillis());
-            Lesson newLesson = articleService.create(lesson);
-            final long articleId = newLesson.getId();
-            phraseService.createPhrase(new PhraseCard(articleId,
-                    "We wait for you.",
-                    "Wir warten auf dich."));
-            phraseService.createPhrase(new PhraseCard(articleId,
-                    "Her family move in a new apartment tomorrow.",
-                    "Ihre Familie ziehen morgen in eine neue Wohnung ein."));
-            phraseService.createPhrase(new PhraseCard(articleId,
-                    "She calls him every Sunday.",
-                    "Sie ruft ihn jeden Sonntag an."));
-            phraseService.createPhrase(new PhraseCard(articleId,
-                    "I go through the park.",
-                    "Ich gehe durch den Park."));
-            phraseService.createPhrase(new PhraseCard(articleId,
-                    "We work about an hour.",
-                    "Wir arbeiten gegen eine Stunde."));
-            phraseService.createPhrase(new PhraseCard(articleId,
-                    "The children sit around the table.",
-                    "Die Kinder sitzen um den Tisch herum."));
-            phraseService.createPhrase(new PhraseCard(articleId,
-                    "I am in a hurry.",
-                    "Ich habe es eilig."));
-            phraseService.createPhrase(new PhraseCard(articleId,
-                    "My apartment consist of 3 rooms.",
-                    "Meine Wohnung bestehet aus 3 Zimmern."));
 
-        };
-    }
 
 }
