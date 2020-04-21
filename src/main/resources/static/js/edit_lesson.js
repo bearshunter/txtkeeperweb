94b929a6ae7aea1360fd6a8ae92dbc56a038580e
Rodjerbear <51483121+Rodjerbear@users.noreply.github.com>
2020-04-21 17:24:23 +0300

    edit lesson front

---
+++ src/main/resources/static/js/edit_lesson.js
@@ -0,0 +1,50 @@
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get('id');
+console.log(document.getElementById('showMore'));
+fetch("/keeper/rest/lesson/" + id)
+    .then(response => response.json())
+    .then(lesson => renderLesson(lesson));
+
+function renderLesson(data) {
+    console.log(data);
+    document.getElementById("headline").value = data.headline;
+    const cards = data.phraseCards;
+    for (let i = 0; i < cards.length; i++) {
+        const card = cards[i];
+        insertRow(card.phrase, card.translation);
+    }
+}
+
+function insertRow(phrase, translation) {
+    const table = document.getElementById("phraseCards").getElementsByTagName('tbody')[0];
+    const newRow = table.insertRow();
+    const cell1 = newRow.insertCell(0);
+    const cell2 = newRow.insertCell(1);
+    cell1.innerHTML = `<input type="text" name="phrase" value="${phrase}">`;
+    cell2.innerHTML = `<input type="text" name="translation" value="${translation}">`;
+
+}
+
+function addLesson() {
+    event.preventDefault();
+    const data = {};
+    data.headline = document.getElementById("headline").value;
+    data.lessonId = id;
+    data.phraseCards = [];
+    const table = document.getElementById("phraseCards");
+    for (let row of table.rows) {
+        const phrase = row.cells[0].firstElementChild.value;
+        const translation = row.cells[1].firstElementChild.value;
+        const phraseCard = {phrase: phrase, translation: translation};
+        data.phraseCards.push(phraseCard);
+    }
+    console.log(data);
+    fetch("/keeper/rest/lesson/" + id, {
+        method: 'PUT',
+            body: JSON.stringify(data),
+        headers: {'Content-type':'application/json; charset=UTF-8'}
+        })
+        .then(response => response.json())
+        .then(data => console.log(data)) // Manipulate the data retrieved back, if we want to do something with it
+        .catch(err => console.log(err)); // Do something with the error
+}
--- src/main/resources/static/js/main1.0.js
+++ src/main/resources/static/js/main1.0.js
@@ -2,6 +2,7 @@ console.log(document.getElementById('showMore'));
 fetch("/keeper/rest/lesson/")
     .then(response => response.json())
     .then(lessons => renderLessons(lessons));
+
 function renderLessons(lessons) {
     const filtered = prepareLes(lessons);
     const container = document.getElementById("container");
@@ -16,15 +17,25 @@ function renderLessons(lessons) {
         const year = createdObj.getFullYear();
         const hours = createdObj.getHours();
         const minutes = createdObj.getMinutes();
-        const linkToArticle = "<div class='lesson' id='" + i + "'><a href='" + "/keeper/lesson.html?id=" + lesson.id + "'>";
-        container.innerHTML = container.innerHTML + linkToArticle + "<p>" + lesson.headline + "</p></a>" + "<p class='date'>" + day + "." + month + "." + year + "</p>" + "<p class='date'>" + hours + ":" + minutes + "</p>" + "<p class='date'>" + lastDate + " Days ago" + "</p>" +
-            "</div>";
+
+        container.innerHTML = container.innerHTML + "<div  id='" + i + "'></div>";
+        const phraseCard = document.getElementById(i);
+        phraseCard.className = "lesson";
+        phraseCard.innerHTML = `<a href='/keeper/lesson.html?id=${lesson.id}'><p>${lesson.headline}</p></a>`;
+        phraseCard.innerHTML = phraseCard.innerHTML + `<p class="date">${day}.${month}.${year}</p>`;
+        phraseCard.innerHTML = phraseCard.innerHTML + `<p class="date">${hours}:${minutes}</p>`;
+        phraseCard.innerHTML = phraseCard.innerHTML + `<p class="date">${lastDate} Days ago</p>`;
+        phraseCard.innerHTML = phraseCard.innerHTML + `<a class="button delete" onclick="deleteLesson(${lesson.id})" href="#"></a>`;
+        phraseCard.innerHTML = phraseCard.innerHTML + `<a class="button edit"  href='/keeper/edit_lesson.html?id=${lesson.id}'></a>`;
+
+
         if (i > 5) {
             document.getElementById(i).style.display = 'none';
             document.getElementById(i).classList.add('hidden');
         }
     }
 }
+
 function prepareLes(lessons) {
     const filteredLes = lessons.filter(a => {
         if (a.headline && a.headline.length > 0) {
@@ -46,28 +57,39 @@ function prepareLes(lessons) {
     console.log(filteredLes);
     return filteredLes;
 }
+
 document.querySelector('#showMore').onclick = function () {
     const hidden = document.getElementsByClassName('hidden');
-    for (  let i = 0; i< hidden.length; i++) {
+    for (let i = 0; i < hidden.length; i++) {
         if (i < 6) {
             hidden[i].style.display = 'inline';
             hidden[i].classList.remove('hidden');
         }
     }
 };
-window.onscroll = function() {
+window.onscroll = function () {
     let scrollElem = document.getElementById("scrollToTop");
-    if ( document.documentElement.scrollTop > document.documentElement.clientHeight) {
+    if (document.documentElement.scrollTop > document.documentElement.clientHeight) {
         scrollElem.style.opacity = "0.5";
     } else {
         scrollElem.style.opacity = "0";
     }
 };
 let timeOut;
+
 function goUp() {
-    let top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
-    if(top > 0) {
-        window.scrollBy(0,-100);
-        timeOut = setTimeout('goUp()',20);
+    let top = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
+    if (top > 0) {
+        window.scrollBy(0, -100);
+        timeOut = setTimeout('goUp()', 20);
     } else clearTimeout(timeOut);
+}
+
+
+function deleteLesson(id) {
+    fetch("/keeper/rest/lesson/" + id, {
+            method: "DELETE"
+        }
+    ).then(res => console.log(res));
+    document.location.reload();
 }
