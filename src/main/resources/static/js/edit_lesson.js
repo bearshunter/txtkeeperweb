8b627570ccf809ec25951311a7b7e9c42884e53c
Rodjerbear <51483121+Rodjerbear@users.noreply.github.com>
2020-05-21 17:35:10 +0300

    sign_up page

--- src/main/resources/static/js/edit_lesson.js
+++ src/main/resources/static/js/edit_lesson.js
@@ -11,13 +11,14 @@ function renderLesson(data) {
     const cards = data.phraseCards;
     for (let i = 0; i < cards.length; i++) {
         const card = cards[i];
-        insertRow(card.phrase, card.translation);
+        insertRow(card.id, card.phrase, card.translation);
     }
 }
 
 function insertRow(phrase, translation) {
     const table = document.getElementById("phraseCards").getElementsByTagName('tbody')[0];
     const newRow = table.insertRow();
+    newRow.id = newRow.rowIndex.toString();
     const cell1 = newRow.insertCell(0);
     const cell2 = newRow.insertCell(1);
     cell1.innerHTML = `<input type="text" name="phrase" value="${phrase}">`;
@@ -39,11 +40,70 @@ function addLesson() {
         data.phraseCards.push(phraseCard);
     }
     console.log(data);
-    fetch("/keeper/rest/lesson/" + id, {
-        method: 'PUT',
-        body: JSON.stringify(data),
-        headers: {'Content-type':'application/json; charset=UTF-8'}
+    if (isValid(data)) {
+        fetch("/keeper/rest/lesson/" + id, {
+            method: 'PUT',
+            body: JSON.stringify(data),
+            headers: {'Content-type': 'application/json; charset=UTF-8'}
         })
-        .then(data => console.log(data)) // Manipulate the data retrieved back, if we want to do something with it
-        .catch(err => console.log(err)); // Do something with the error
+            .then(openModal) // Manipulate the data retrieved back, if we want to do something with it
+            .catch(err => console.log(err)); // Do something with the error
+    } else {
+        showError();
+    }
+
+
+}
+
+function openModal() {
+    document.getElementById("myModal").style.display = 'block';
+}
+
+function closeModal() {
+    document.getElementById("myModal").style.display = 'none';
+}
+
+function backToLessons() {
+    window.location.href = "/keeper/index.html";
+}
+
+function addLine() {
+    const table = document.getElementById("phraseCards").getElementsByTagName('tbody')[0];
+    const newRow = table.insertRow();
+    newRow.id = newRow.rowIndex.toString();
+    const cell1 = newRow.insertCell(0);
+    const cell2 = newRow.insertCell(1);
+    cell1.innerHTML = "<input type=\"text\" name=\"phrase\" >";
+    cell2.innerHTML = "<input type=\"text\" name=\"transation\" >";
+    console.log("penis");
+}
+
+function isValid(data) {
+    //is headline valid
+    const headline = data.headline;
+    let valid = true;
+
+    if (headline === "") {
+        valid = false;
+    }
+    for (let i = 0; i < data.phraseCards.length; i++) {
+        const row = data.phraseCards[i];
+        const phrase = row.phrase;
+        const translation = row.translation;
+        const tr = document.getElementById(i.toString());
+        //is rows valid
+        if (phrase === "") {
+            tr.firstElementChild.firstElementChild.style.borderColor = 'red';
+            valid = false;
+        }
+        if (translation === "") {
+            tr.lastElementChild.firstElementChild.style.borderColor = 'red';
+            valid = false;
+        }
+    }
+    return valid;
+}
+
+function showError() {
+    alert("Data is not valid");
 }
--- src/main/resources/static/js/lesson.js
+++ src/main/resources/static/js/lesson.js
@@ -1,25 +1,35 @@
-$(document).ready(function () {
-    const urlParams = new URLSearchParams(window.location.search);
-    const id = urlParams.get('id');
-    $.get("/keeper/rest/lesson/" + id, function (data, status) {
-        headline.innerHTML = data.headline;
-        const phraseArray = data.phraseCards;
-        for (let i = 0; i < phraseArray.length; i++) {
-            const phrase = phraseArray[i];
-            insertRow(phrase.phrase, phrase.translation);
-        }
-
-        reset();
-    });
-
-    document.getElementById("reset").onclick = reset;
-
-});
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get('id');
 
 
-function insertRow(phrase, translation) {
+document.getElementById("reset").onclick = reset;
+document.getElementById('next').onclick = next;
+document.getElementById('closeModal').onclick = closeModal;
+let CARD_INDEX = 0 ;
+
+fetch("/keeper/rest/lesson/" + id)
+    .then(response => response.json())
+    .then(lesson => renderLesson(lesson));
+
+function renderLesson(data) {
+    const headline = document.getElementById("headline");
+    headline.innerHTML = data.headline;
+    const phraseArray = data.phraseCards;
+    for (let i = 0; i < phraseArray.length; i++) {
+        const phrase = phraseArray[i];
+        insertRow(phrase.phrase, phrase.translation, i);
+    }
+
+    reset();
+}
+
+function insertRow(phrase, translation, i) {
     const table = document.getElementById("phraseCards").getElementsByTagName('tbody')[0];
     const newRow = table.insertRow();
+
+    if (i!==0){
+        newRow.style.display = 'none';
+    }
     const cell1 = newRow.insertCell(0);
     const cell2 = newRow.insertCell(1);
     cell1.innerHTML = phrase;
@@ -40,4 +50,29 @@ function reset() {
         translations[i].style.visibility = 'hidden';
 
     }
+
+    const table = document.getElementById("phraseCards");
+    table.rows[CARD_INDEX].style.display ='none';
+    table.rows[0].style.display ='';
+    CARD_INDEX = 0;
+
+}
+function next() {
+    const table = document.getElementById("phraseCards");
+    const  l = table.rows.length;
+    if (CARD_INDEX === l-1){
+        document.getElementById("myModal").style.display = 'block';
+
+    }  else {
+        table.rows[CARD_INDEX].style.display ='none';
+        CARD_INDEX ++;
+        table.rows[CARD_INDEX].style.display ='';
+    }
+
+
+
+
+}
+function closeModal() {
+    window.location.reload(false);
 }
